fae8fce5d51dac5ca97d1eeb0b07848988b1987a
Erik Bartlow <Ebartlow@versonasystems.com>
2024-04-18 09:59:38 -0500

    Committing updates to keep them.  This branch most likely won't go forward.

---
+++ ColumnSchema.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodedThought.Core.Data.Interfaces;
+
+namespace CodedThought.Core.Data.SqlServer
+{
+    [DataTable("COLUMNS", "", "INFORMATION_SCHEMA")]
+    public class ColumnSchema : IColumnSchema
+    {
+        [DataColumn("COLUMN_NAME", System.Data.DbType.String)]
+        public string ColumnOwner { get; set; }
+        [DataColumn("ORDINAL_POSITION", System.Data.DbType.Int32)]
+        public string ColumnNumber { get; set; }
+        [DataColumn("COLUMN_NAME", System.Data.DbType.String)]
+        public string ColumnName { get; set; }
+        [DataColumn("DATA_TYPE", System.Data.DbType.String)]
+        public string ColumnTypeName { get; set; }
+        public DbTypeSupported ColumnType { get; }
+        [DataColumn("IS_NULLABLE", System.Data.DbType.String)]
+        public bool IsNullable { get; set; }
+        [DataColumn("IS_IDENTITY", System.Data.DbType.String)]
+        public bool IsIdentity { get; set; }
+        [DataColumn("CHARACTER_MAXIMUM_LENGTH", System.Data.DbType.String)]
+        public int MaxLength {  get; set; }
+        [DataColumn("ORDINAL_POSITION", System.Data.DbType.String)]
+        public int OrdinalPosition {  get; set; }
+      
+    }
+}
